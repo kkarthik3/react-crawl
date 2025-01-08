316fb62a00987c33bbcfb711e5f2058fa297024c
Ponkothandaraman <126084280+Ponkothandaraman@users.noreply.github.com>
2025-01-08 17:37:50 +0530

    Updated api

--- src/components/DataIngestion.tsx
+++ src/components/DataIngestion.tsx
@@ -42,7 +42,7 @@ export default function SmartProductSearch() {
         throw new Error("Maximum 5 URLs allowed");
       }
 
-      const response = await fetch("http://localhost:8000/", {
+      const response = await fetch("https://scrape-graph-api-dev.ispgnet.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -73,7 +73,7 @@ export default function SmartProductSearch() {
     selectedDatabase: string
   ) => {
     try {
-      const response = await fetch("http://localhost:8000/ingest", {
+      const response = await fetch("https://scrape-graph-api-dev.ispgnet.com/ingest", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
